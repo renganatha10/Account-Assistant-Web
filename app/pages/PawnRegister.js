af8a46cdf1f0e34ff887ce11b0a049f1b0988875
Renganatha A <renganatha@sahusoft.com>
2016-07-24 23:42:26 +0530

    Added Electron Support

--- app/pages/PawnRegister.js
+++ app/pages/PawnRegister.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class PawnRegister extends Component {
 
   render() {
-    var style = {position : 'absolute',bottom : 20, right : 20}
+    var style = { position: 'absolute', bottom: 20, right: 20 }
     return (
       <div className="container-fluid">
         <table className="table table-striped">
@@ -76,6 +76,53 @@ export default class PawnRegister extends Component {
           <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
         </button>
 
+
+        <div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times; </span></button>
+                <h4 className="modal-title" id="myModalLabel">Filter And Search</h4>
+              </div>
+              <div className="modal-body">
+                <form className="form-horizontal">
+                  <div className="form-group">
+                    <label className="col-xs-4 control-label" htmlFor="name">பெயர்</label>
+                    <div className="col-xs-8">
+                      <input type="text" className="form-control" id="name" placeholder="பெயர்" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label className="col-xs-4 control-label" htmlFor="name">கடன் தொகை</label>
+                    <div className="col-xs-8">
+                      <input type="text" className="form-control" id="name" placeholder="கடன் தொகை" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label className="col-xs-4 control-label" htmlFor="name">தேதி</label>
+                    <div className="col-xs-8" id="todate">
+                      <input type="text" className="form-control" placeholder="நொட்டிஸ் தேதி" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label className="col-xs-4 control-label" htmlFor="name">நொட்டிஸ் தேதி</label>
+                    <div className="col-xs-8" id="todate">
+                      <input type="text" className="form-control" placeholder="நொட்டிஸ் தேதி" />
+                    </div>
+                  </div>
+                  <div className="flex-container">
+                    <button type="submit" className="btn btn-success btn-lg">தெடுக</button>
+                    <button style={{marginLeft : 10} } className="btn btn-default btn-lg">Reset</button>
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
